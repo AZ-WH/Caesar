8ff946c5149614c4854d23369dfb602159b5fb8c
吴辉 <wuhui904107775@qq.com>
2016-08-26 14:23:25 +0800

    路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,5 +16,5 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => ['api'] ], function () {
-    Route::controllers('/wechat' , 'WechatPubController');
+    Route::controller('/wechat' , 'WechatPubController' , ['anyToken' => 'wechat.token']);
 });
