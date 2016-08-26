ad5118667490fc07b2b9d3425b0eb4ef18ca7a11
吴辉 <wuhui904107775@qq.com>
2016-08-26 14:40:27 +0800

    微信

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -328,7 +328,7 @@ class ComposerStaticInitc1eb78dbffde30c94271d3feb133c2a1
         'App\\Http\\Controllers\\Auth\\AuthController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/AuthController.php',
         'App\\Http\\Controllers\\Auth\\PasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/PasswordController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
-        'App\\Http\\Controllers\\WechatPubController' => __DIR__ . '/../..' . '/app/Http/Controllers/WechatPubController.php',
+        'App\\Http\\Controllers\\WechatController' => __DIR__ . '/../..' . '/app/Http/Controllers/WechatController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
